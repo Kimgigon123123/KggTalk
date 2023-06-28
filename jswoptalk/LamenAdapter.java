174457c7d5572a3f2cc5374f5d5cc0db2f3bb7d0
Jeongsuwon <129022812+Jeongsuwon@users.noreply.github.com>
2023-06-28 18:34:50 +0900

    정수원 기능1
    ㅁㄴㅇ

---
+++ jswoptalk/LamenAdapter.java
@@ -0,0 +1,55 @@
+package com.example.project01_kggtalk.opentalk;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project01_jswtalk.databinding.ItemLamenRecvBinding;
+
+import java.util.ArrayList;
+
+public class LamenAdapter extends RecyclerView.Adapter<LamenAdapter.ViewHolder>{
+
+    ItemLamenRecvBinding binding;
+    ArrayList<LamenDTO> list;
+    Context context;
+
+    public LamenAdapter(ArrayList<LamenDTO> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        binding = ItemLamenRecvBinding.inflate(inflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder h, int i) {
+            h.binding.imgvCategroy1.setImageResource(list.get(i).getImgRes());
+            h.binding.tvLamenTitle1.setText(list.get(i).getTitle());
+            h.binding.tvLamenInfo1.setText(list.get(i).getPeople());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ItemLamenRecvBinding binding;
+
+        public ViewHolder(@NonNull ItemLamenRecvBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
---
+++ jswoptalk/ListenDTO.java
@@ -0,0 +1,137 @@
+package com.example.project01_kggtalk.opentalk;
+
+public class ListenDTO {
+    private int imgHeadRes, imgChatRes1, imgChatRes2, imgChatRes3, imgRoomRes1, imgRoomRes2, imgRoomRes3;
+    private String head, chatTitle1, chatPeople1, chatTitle2, chatPeople2, chatTitle3, chatPeople3;
+
+    public int getImgHeadRes() {
+        return imgHeadRes;
+    }
+
+    public void setImgHeadRes(int imgHeadRes) {
+        this.imgHeadRes = imgHeadRes;
+    }
+
+    public int getImgChatRes1() {
+        return imgChatRes1;
+    }
+
+    public void setImgChatRes1(int imgChatRes1) {
+        this.imgChatRes1 = imgChatRes1;
+    }
+
+    public int getImgChatRes2() {
+        return imgChatRes2;
+    }
+
+    public void setImgChatRes2(int imgChatRes2) {
+        this.imgChatRes2 = imgChatRes2;
+    }
+
+    public int getImgChatRes3() {
+        return imgChatRes3;
+    }
+
+    public void setImgChatRes3(int imgChatRes3) {
+        this.imgChatRes3 = imgChatRes3;
+    }
+
+    public int getImgRoomRes1() {
+        return imgRoomRes1;
+    }
+
+    public void setImgRoomRes1(int imgRoomRes1) {
+        this.imgRoomRes1 = imgRoomRes1;
+    }
+
+    public int getImgRoomRes2() {
+        return imgRoomRes2;
+    }
+
+    public void setImgRoomRes2(int imgRoomRes2) {
+        this.imgRoomRes2 = imgRoomRes2;
+    }
+
+    public int getImgRoomRes3() {
+        return imgRoomRes3;
+    }
+
+    public void setImgRoomRes3(int imgRoomRes3) {
+        this.imgRoomRes3 = imgRoomRes3;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getChatTitle1() {
+        return chatTitle1;
+    }
+
+    public void setChatTitle1(String chatTitle1) {
+        this.chatTitle1 = chatTitle1;
+    }
+
+    public String getChatPeople1() {
+        return chatPeople1;
+    }
+
+    public void setChatPeople1(String chatPeople1) {
+        this.chatPeople1 = chatPeople1;
+    }
+
+    public String getChatTitle2() {
+        return chatTitle2;
+    }
+
+    public void setChatTitle2(String chatTitle2) {
+        this.chatTitle2 = chatTitle2;
+    }
+
+    public String getChatPeople2() {
+        return chatPeople2;
+    }
+
+    public void setChatPeople2(String chatPeople2) {
+        this.chatPeople2 = chatPeople2;
+    }
+
+    public String getChatTitle3() {
+        return chatTitle3;
+    }
+
+    public void setChatTitle3(String chatTitle3) {
+        this.chatTitle3 = chatTitle3;
+    }
+
+    public String getChatPeople3() {
+        return chatPeople3;
+    }
+
+    public void setChatPeople3(String chatPeople3) {
+        this.chatPeople3 = chatPeople3;
+    }
+
+    public ListenDTO(int imgHeadRes, int imgChatRes1, int imgChatRes2, int imgChatRes3, int imgRoomRes1, int imgRoomRes2, int imgRoomRes3, String head, String chatTitle1, String chatPeople1, String chatTitle2, String chatPeople2, String chatTitle3, String chatPeople3) {
+        this.imgHeadRes = imgHeadRes;
+        this.imgChatRes1 = imgChatRes1;
+        this.imgChatRes2 = imgChatRes2;
+        this.imgChatRes3 = imgChatRes3;
+        this.imgRoomRes1 = imgRoomRes1;
+        this.imgRoomRes2 = imgRoomRes2;
+        this.imgRoomRes3 = imgRoomRes3;
+        this.head = head;
+        this.chatTitle1 = chatTitle1;
+        this.chatPeople1 = chatPeople1;
+        this.chatTitle2 = chatTitle2;
+        this.chatPeople2 = chatPeople2;
+        this.chatTitle3 = chatTitle3;
+        this.chatPeople3 = chatPeople3;
+
+
+    }
+}
